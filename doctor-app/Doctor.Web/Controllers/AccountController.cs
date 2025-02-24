e8bc54bcfc264853e2bbc732b8b659a7f7aef99a
sbalak <sidharth.balakrishnan@outlook.com>
2025-02-24 22:46:56 +0530

    sign up page added

---
+++ doctor-app/Doctor.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Doctor.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
